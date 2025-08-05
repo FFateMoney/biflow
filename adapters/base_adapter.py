3967c3d478127bdd11acee0e6d8465d31a887e54
玖辞. <2649299591@qq.com>
2025-08-05 13:37:01 +0000

    update adapters/base_adapter.py.
    Signed-off-by: 玖辞. <2649299591@qq.com>

--- adapters/base_adapter.py
+++ adapters/base_adapter.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
-
 from core.node import WorkflowNode
 
-
 class BaseAdapter(ABC):
+    def __init__(self, config=None, sample_data=None):
+        self.config = config or {}  # 默认空字典
+        self.sample_data = sample_data
+
     @abstractmethod
     def adapt(self, node: WorkflowNode) -> WorkflowNode:
-        """
+     """
         接收一个 WorkflowNode，根据 node.pararm 拼接命令行，
         更新 node.commands 并返回。
         """
