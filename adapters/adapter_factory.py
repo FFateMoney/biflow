028189225ff0c8e04dba7ba81a42da90f17b3d6c
玖辞. <2649299591@qq.com>
2025-09-02 07:40:25 +0000

    update adapters/adapter_factory.py.
    Signed-off-by: 玖辞. <2649299591@qq.com>

--- adapters/adapter_factory.py
+++ adapters/adapter_factory.py
@@ -1,33 +1,37 @@
-# adapters/adapter_factory.py
-
-from adapters.samtools_adapter import SamtoolsAdapter
-from adapters.test_adapter import TestAdapter
-from adapters.fastqc_adapter import FastQCAdapter
-from adapters.multiqc_adapter import MultiQCAdapter
-from adapters.trimgalore_adapter import TrimGaloreAdapter
-from adapters.bwa_adapter import BwaAdapter
-from adapters.bowtie2_adapter import Bowtie2Adapter
-from adapters.picard_adapter import PicardAdapter
 from core.node import WorkflowNode
+#from adapters.samtools_adapter import SamtoolsAdapter
+from adapters.test_adapter import TestAdapter
+#from adapters.fastqc_adapter import FastQCAdapter
+#from adapters.multiqc_adapter import MultiQCAdapter
+#from adapters.trimgalore_adapter import TrimGaloreAdapter
+#from adapters.bwa_adapter import BwaAdapter
+#from adapters.bowtie2_adapter import Bowtie2Adapter
+#from adapters.picard_adapter import PicardAdapter
+from adapters.vcftools_adapter import VcftoolsAdapter
+from adapters.plink_adapter import PlinkAdapter
+from adapters.hapmap_adapter import HapmapAdapter
+
+ADAPTER_MAP = {
+    #"samtools": SamtoolsAdapter,
+   # "samtools_sort": SamtoolsAdapter,
+    "test": TestAdapter,
+   # "fastqc": FastQCAdapter,
+   #"multiqc": MultiQCAdapter,
+   # "trim_galore": TrimGaloreAdapter,
+    #"bwa_index": BwaAdapter,
+    #"bwa_mem": BwaAdapter,
+    #"bwa": BwaAdapter,
+    #"bowtie2": Bowtie2Adapter,
+    #"picard_markduplicates": PicardAdapter,
+    #"picard_addrg": PicardAdapter,
+    #"picard": PicardAdapter,
+    "vcftools": VcftoolsAdapter,
+    "plink": PlinkAdapter,
+    "hapmap": HapmapAdapter,
+}
 
 def get_adapter(node: WorkflowNode):
-    tool = node.tool.lower()  # 小写防止大小写问题
-
-    adapter_map = {
-        "samtools": SamtoolsAdapter,
-        "test": TestAdapter,
-        "fastqc": FastQCAdapter,
-        "multiqc": MultiQCAdapter,
-        "trim_galore": TrimGaloreAdapter,
-        "bowtie2": Bowtie2Adapter,
-        "bwa": BwaAdapter,
-        "samtools_sort": SamtoolsAdapter,
-        "picard": PicardAdapter,
-        # "bwa": BwaAdapter,
-        # "bcftools": BcftoolsAdapter,
-    }
-
-    if tool not in adapter_map:
+    tool = node.tool.lower()
+    if tool not in ADAPTER_MAP:
         raise ValueError(f"No adapter found for tool: '{tool}'")
-
-    return adapter_map[tool]()
+    return ADAPTER_MAP[tool]()
