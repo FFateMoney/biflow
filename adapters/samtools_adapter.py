6248749c4ccf580a369e9d28d92dd0663397596e
玖辞. <2649299591@qq.com>
2025-09-22 10:57:08 +0000

    update adapters/samtools_adapter.py.
    Signed-off-by: 玖辞. <2649299591@qq.com>

--- adapters/samtools_adapter.py
+++ adapters/samtools_adapter.py
@@ -4,13 +4,18 @@ from pathlib import Path
 
 class SamtoolsAdapter(BaseAdapter):
     def __init__(self, config=None, sample_data=None):
-        super().__init__(config, sample_data)
+        super().__init__(config or {}, sample_data)
 
     def adapt(self, node: WorkflowNode) -> WorkflowNode:
         operation = node.name.lower()
-        if operation in ("batch_sorting", "samtools_sort"):
-            return self._build_sort(node)
-        raise ValueError(f"Unsupported samtools operation: {operation}")
+        operation_map = {
+            "batch_sorting" : self._build_sort,
+            "samtools_sort" : self._build_sort,
+            "samtools_fadix": self._samtools_faidx
+        }
+        if operation not in operation_map:
+            raise ValueError(f"Unsupported samtools operation: {operation}")
+        return operation_map[operation](node)
 
     def _build_sort(self, node: WorkflowNode):
         samtools_path = node.params["samtools_path"]
@@ -40,3 +45,32 @@ class SamtoolsAdapter(BaseAdapter):
                 )
         node.commands = commands
         return node
+
+
+    def _samtools_faidx(self,node: WorkflowNode):
+        samtools_path: Path = node.params.get("tool_path")
+        intput_path: Path = (node.input_dir.get("reference") / node.params.get("reference")).as_posix()
+        command = [
+            samtools_path,"faidx",intput_path
+        ]
+        node.commands.append(command)
+        return node
+
+    def _samtools_local_realignment(self,node: WorkflowNode):
+        samtools_path: Path = node.params.get("tool_path")
+        input_path: Path = node.input_dir.get("bam")
+        output_path = node.output_dir
+        bam_files = list(Path(input_path).glob("*.bam"))
+        th = node.params.get("th")
+        commands = []
+        for bam in bam_files:
+            bam = input_path/bam
+            bam_str = bam.as_posix()
+            output = output_path/bam_str.replace("bam","bai")
+            command = [samtools_path,"index","-@",th,bam,output]
+            commands.append(command)
+        node.commands = commands
+        return node
+
+
+
